8f95c8dad8c84b703838caadcc9f270195ebcf3d
Fábio Tramasoli <fabio@tramasoli.com>
2017-12-04 10:09:19 -0200

    Aula01 - Exemplos de arrow functions

--- aula01/app.ts
+++ aula01/app.ts
@@ -4,3 +4,24 @@
 
 let message:string = 'Primeira frase';
 console.log(message);
+
+function sum(a: number, b: number): number {
+    return a+b;
+}
+console.log(sum(1, 2));
+
+function increment(a: number, b?:number): number {
+    return a+(b||1);
+}
+console.log(increment(1));
+
+function divide(a: number, b:number=1): number {
+    return a/b;
+}
+console.log(divide(2,2));
+
+const sum2 = (a: number, b:number):number => a+b;
+console.log(sum2(2,2));
+
+const sumArray = (numbers: number[]) :number => numbers.reduce((total, item) => total+item, 0)
+console.log(sumArray([1,2,3,4,5]));
