c2d3c36ae63265efc1aff16977fa7a72c08f0e92
Fábio Tramasoli <fabio@tramasoli.com>
2017-12-05 09:13:29 -0200

    Aula02 - cria componentes 'footer' e 'home'

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
